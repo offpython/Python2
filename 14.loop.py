5ba09637e2a9261d9f4d4d0a5cba6ec92c18cef7
offpython <offpython@gmail.com>
2023-10-01 14:57:27 +0900

    review

--- 14.loop.py
+++ 14.loop.py
@@ -1,2 +1,20 @@
-# 횟수에 의한 반복 for문
+# 횟수에 의한 반복 (for문)
 
+for i in range(1, 10):
+    result = 7 * i
+    print('{} * {} = {}'.format(7, i, result))
+
+for k in range(100):
+    pass
+
+for h in range(5):
+    print('Hi', end='')
+    print(' loop statement')
+
+for num in range(5):
+    print('Hello Python')
+
+gugudanNum = int(input('구구단: '))
+for num in range(1, 10):
+    result = gugudanNum * num
+    print('{} * {} = {}'.format(gugudanNum, num, result))
