df6f82dc5985ef7f0092958ef46d44e032b261c1
offpython <offpython@gmail.com>
2023-11-14 09:55:04 +0900

    review

--- 20.loop.py
+++ 20.loop.py
@@ -1,50 +1,17 @@
-num = 0
-
-while True:
-    print('hello ~~ ')
-
-    num += 1
-    if num >= 5:
-        break
-
-print('The End')
-
-sum = 0
-searchNum = 0
-
-for i in range(100):
-    sum += i
-
-    if sum > 100:
-        searchNum = i
-        break
-
-print('searchNum : {}'.format(searchNum))
+for i in range(1, 10):
+    for j in range(i):
+        print('*', end='')
+    print()
 
 # 실습1
-
-result = 1
-num = 0
-
-for i in range(1, 11):
-    result *= i
-
-    if result > 50:
-        num = i
-        break
-
-print('num : {}, result: {}'.format(num, result))
+for i in range(10, 0, -1):
+    for j in range(i):
+        print('*', end='')
+    print()
 
 # 실습2
-limitWeight = 2200
-currentWeight = 800
-date = 1
-
-while True:
-    if currentWeight >= limitWeight:
-        break
-
-    date += 1
-    currentWeight += 70
-
-print('이유식 중단 날짜: {}'.format(date))
+for i in range(1, 10):
+    for j in range(2, 10):
+        result = j * i
+        print('{} * {} = {}\t'.format(j, i, result), end='')
+    print()
--- 21.def.py
+++ 21.def.py
@@ -11,3 +11,29 @@ def calFun():
     n2 = int(input('n2 입력 : '))
     print(f'n1 * n1 = {n1 * n2}')
     print(f'n1 / n1 = {round(n1 / n2, 2)}')
+
+def fun1():
+    print('fun1 호출')
+    fun2()
+    print('fun2 호출 후에 실행!')
+def fun2():
+    print('fun2 호출')
+    fun3()
+def fun3():
+    print('fun3 호출')
+
+def printTodayWeather():
+    pass
+
+def gugudan2():
+    for i in range(1, 10):
+        print(f'2 * {i} = {i * 2}')
+    gugudan3()
+def gugudan3():
+    for i in range(1, 10):
+        print(f'3 * {i} = {i * 3}')
+    gugudan5()
+def gugudan5():
+    for i in range(1, 10):
+        print(f'5 * {i} = {i * 5}')
+
