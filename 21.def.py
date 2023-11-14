3e6a6209959e52c3bf1668a58eb30ce3e5a7a7a7
offpython <offpython@gmail.com>
2023-11-14 10:07:31 +0900

    review

--- 21.def.py
+++ 21.def.py
@@ -37,3 +37,29 @@ def gugudan5():
     for i in range(1, 10):
         print(f'5 * {i} = {i * 5}')
 
+def greet(customer1, customer2):
+    print(f'{customer1}, {customer2}님, 안녕하세요.')
+
+def calculator(n1, n2):
+    print(f'{n1} + {n2} = {n1 + n2}')
+    print(f'{n1} - {n2} = {n1 - n2}')
+    print(f'{n1} * {n2} = {n1 * n2}')
+    print(f'{n1} / {n2} = {n1 / n2}')
+
+def printNumber(*numbers):
+    for number in numbers:
+        print(number, end='')
+    print()
+
+
+def printScore(kor, eng, mat):
+    sum = kor + eng + mat
+    avg = sum / 3
+
+    print(f'총점 : {sum}')
+    print(f'평균 : {round(avg, 2)}')
+
+# korScore = int(input('국어 점수 입력 : '))
+# engScore = int(input('영어 점수 입력 : '))
+# matScore = int(input('수학 점수 입력 : '))
+# printScore(korScore, engScore, matScore)
